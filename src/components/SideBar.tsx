e2acba2ccf065b802355412fb96a4f224cb28645
Mabson Vinicius <mabson@spmmacbookti01.local>
2022-04-07 17:24:05 -0300

    first commit

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+
+import { Button } from './Button';
+
+import { api } from '../services/api';
+
+import '../styles/global.scss';
+
+import '../styles/sidebar.scss';
+import '../styles/content.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  selectedGenreId: number;
+  handleClickButton(id: number): any;
+}
+
+export function SideBar({ selectedGenreId, handleClickButton }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Cafas<p>Watch</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
